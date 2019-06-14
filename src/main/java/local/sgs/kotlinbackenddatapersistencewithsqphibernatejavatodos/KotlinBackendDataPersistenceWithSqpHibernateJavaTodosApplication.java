d51784252d392c2d828287fcaf5c7b888276c067
Spencer Stock <spencerstock@gmail.com>
2019-06-14 09:23:59 -0600

    initial commit

---
+++ src/main/java/local/sgs/kotlinbackenddatapersistencewithsqphibernatejavatodos/KotlinBackendDataPersistenceWithSqpHibernateJavaTodosApplication.java
@@ -0,0 +1,13 @@
+package local.sgs.kotlinbackenddatapersistencewithsqphibernatejavatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinBackendDataPersistenceWithSqpHibernateJavaTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinBackendDataPersistenceWithSqpHibernateJavaTodosApplication.class, args);
+    }
+
+}
